ec72f302f0ac6463efa8f6d25eaca4acb71b87af
Trond Klakken <trond@klakken.com>
2019-06-01 23:20:12 +0200

    Change number of lucky sevens
    Pictures missing in dropdown menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,62 +24,25 @@
       </v-toolbar-title>
       
       <v-spacer></v-spacer>
-      <v-menu offset-y>
-        <template v-slot:activator="{ onLucky }">
-        <v-btn
-          class="change-lucky-7"
-          title="Change number of lucky sevens"
-          right
-          large
-          fab
-          flat
-          dark
-          v-on="onLucky"
-          >
-          <v-avatar
-            size="45"
-            tile
-            class="text-lowercase"
-            >
-            <span class="multiplier">
-              <span class="text-large">
-                {{numberOfLuckySeven}}
-              </span>
-              <span class="times">
-                x
-              </span>
-            </span>
-            <v-img
-              :src="require('@/assets/icons/icon_succes_7.png')"
-              class="lucky7"
-              contain
-            ></v-img>
-          </v-avatar>
-        </v-btn>
-      </template>
-        
-      <v-list>
-        <v-list-tile
-          v-for="number of possibleLucky7ns"
-          :key="number"
-          @click="changeNumberOfLuckySeven(number)" 
+     
+      <NumberOfLuckySevenElem
+        :numberOfLuckySeven="numberOfLuckySeven"
+        @changeNumberOfLuckySeven="changeNumberOfLuckySeven"
         >
-          <v-list-tile-title>{{ number }}</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-      </v-menu>
+      </NumberOfLuckySevenElem>
     </v-toolbar>
 </template>
 
 <script>
   import NumberOfPlayersElem from './NumberOfPlayers'
+  import NumberOfLuckySevenElem from './NumberOfLuckySeven'
 
   export default {
     components: {
-      NumberOfPlayersElem
+      NumberOfPlayersElem,
+      NumberOfLuckySevenElem
     },
     data: () => ({
-      possibleLucky7ns: [ 1, 2, 3, 4, 5, 6 ],
     }),
 
     props: [
@@ -99,13 +62,6 @@
 </script>
 
 <style>
-  .text-large {
-    font-size: large;
-  }
-
-  .times {
-    padding: 0.15em;
-  }
 
   .header {
     background-image: url(../assets/menu_bar.png);
---
+++ src/components/NumberOfLuckySeven.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="text-xs-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          flat
+          v-on="on"
+          class="change-lucky-7"
+          title="Change number of lucky sevens"
+          right
+          large
+          fab
+          dark
+        >
+          <v-avatar
+            size="45"
+            tile
+            >
+            <span class="multiplier">
+              <span class="text-large">
+                {{numberOfLuckySeven}}
+              </span>
+              <span class="times">
+                x
+              </span>
+            </span>
+            <v-img
+              :src="require('@/assets/icons/icon_succes_7.png')"
+              class="lucky7"
+              contain
+            ></v-img>
+          </v-avatar>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-tile
+          v-for="number of possibleLucky7ns"
+          :key="number"
+          @click="changeNumberOfLuckySeven(number)"
+        >
+          <v-list-tile-title>
+            <v-avatar
+            tile
+            >
+            <span class="multiplier">
+              <span class="">
+                {{number}}
+              </span>
+              <span class="times">
+                x
+              </span>
+            </span>
+          </v-avatar>
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["numberOfLuckySeven"],
+
+  data() {
+      return {
+        possibleLucky7ns: [ 1, 2, 3, 4, 5, 6 ],
+      }
+  },
+
+  methods: {
+    changeNumberOfLuckySeven(number) {
+      this.$emit('changeNumberOfLuckySeven', number)
+    },
+  }
+};
+</script>
+
+<style>
+  .text-large {
+    font-size: large;
+  }
+
+  .times {
+    padding: 0.15em;
+  }
+
+  .lucky7__small {
+    height: 1em;
+  }
+
+</style>
